f324cac20c5e547dc9f63c45479d814d0a9dbde5
clq5666 <clq5666@9gna.a1p.me>
2021-10-27 12:23:25 +0800

    代码清理

--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Data;
-using System.Data.Common;
-using System.Data.OleDb;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace 数据库扩展
